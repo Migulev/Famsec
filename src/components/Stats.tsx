4d6dc7d4f1e46cd111ab879d35810250c19681f6
Migulev <Migulev@yandex.ru>
2023-08-17 11:35:38 -0400

    + Stats section

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import { BiChevronRight } from 'react-icons/bi';
 
 const statsContent = {
@@ -11,11 +13,11 @@ const statsContent = {
       label: 'Happy Customers',
     },
     {
-      number: '4.5',
+      number: '4.8',
       label: 'Rating Review',
     },
   ],
-  getStarted: {
+  content: {
     heading: 'Get started with our service?',
     description:
       'We understand that protecting your loved ones is of utmost importance',
@@ -29,25 +31,40 @@ const statsContent = {
 
 const Stats = () => {
   return (
-    <section className=" pb-10 pt-20">
-      <div className=" container mx-auto px-4">
-        <div className=" items-center justify-between gap-9 lg:flex">
-          <div className=" mb-20 w-full lg:mb-0 lg:w-7/12">
-            <div className=" grid grid-cols-3">
-              {statsContent.stats.map((stat) => (
-                <div className=" text-center lg:text-left" key={stat.label}>
-                  <strong className=" block text-4xl font-bold leading-tight text-primary xl:text-[52px]">
-                    {stat.number}
-                  </strong>
-                  <span>{stat.label}</span>
-                </div>
-              ))}
+    <section className=" container mx-auto items-center justify-between gap-9 px-4 pb-10 pt-20 lg:flex">
+      <div className=" mb-20 w-full lg:mb-0 lg:w-7/12">
+        <div className=" grid grid-cols-3">
+          {statsContent.stats.map((item) => (
+            <div className=" text-center lg:text-left" key={item.label}>
+              <strong className=" block text-4xl font-bold leading-tight text-primary xl:text-[52px]">
+                {item.number}
+              </strong>
+              <span>{item.label}</span>
             </div>
-          </div>
-          <div className=" w-full lg:w-5/12">
-            <div className=""></div>
-          </div>
+          ))}
         </div>
+      </div>
+      <div className=" relative rounded-lg bg-light px-7 py-10 pr-28 md:pr-32 lg:w-5/12 lg:px-10 lg:pr-40">
+        <Image
+          src={statsContent.content.img}
+          alt="img"
+          width={1}
+          height={1}
+          className=" absolute -top-14 right-0 w-24 lg:right-6"
+        />
+        <h3 className=" mb-3 text-xl font-bold text-heading">
+          {statsContent.content.description}
+        </h3>
+        <p className=" mb-5 text-base">{statsContent.content.heading}</p>
+        <Link
+          href={statsContent.content.cta.cta_href}
+          className=" group flex items-center gap-2 font-semibold text-primary outline-none"
+        >
+          <span>{statsContent.content.cta.cta_label}</span>
+          <span className="inline-flex w-6 items-center justify-center rounded-full bg-primary text-white transition-all duration-300 ease-in-out group-hover:bg-secondary">
+            <BiChevronRight className=" text-lg" />
+          </span>
+        </Link>
       </div>
     </section>
   );
